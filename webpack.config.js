e5468bdad815f87e77849c63e7ab0dd9fdb3cfe4
Luis Reis <luis.m.reis@gmail.com>
2015-08-22 12:08:04 +0100

    Add clean webpack plugin.

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,18 @@
 var path = require('path');
+var Clean = require('clean-webpack-plugin');
+
+const distDir = 'dist';
 
 module.exports = {
   entry: [
     './app/index'
   ],
   output: {
-    path: path.join(__dirname, 'dist'),
+    path: path.join(__dirname, distDir),
     filename: 'bundle.js',
     publicPath: '/static/'
-  }
+  },
+  plugins: [
+    new Clean([distDir])
+  ]
 };
