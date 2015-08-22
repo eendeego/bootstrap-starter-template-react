2b6b7c539d0dac9558e0783fbe28158945c995de
Luis Reis <luis.m.reis@gmail.com>
2015-08-22 21:19:15 +0100

    Add bootstrap-sass.

--- app/index.js
+++ app/index.js
@@ -1,3 +1,1 @@
-import './styles/application.scss';
-
 console.log('Hello world!');
--- webpack.config.js
+++ webpack.config.js
@@ -11,12 +11,21 @@ module.exports = {
   entry: [
     'webpack-dev-server/client?http://localhost:' + (process.env.WDS_PORT || 3000),
     'webpack/hot/only-dev-server',
+    'bootstrap-sass!./bootstrap-sass.config.js',
     './app/index'
   ],
   module: {
     loaders: [
       { test: /\.js$/, loaders: ['react-hot', 'babel'], exclude: /node_modules/ },
-      { test: /\.scss$/, loader: ExtractTextPlugin.extract('css?sourceMap!sass?sourceMap') }
+      { test: /\.scss$/, loader: ExtractTextPlugin.extract('css?sourceMap!sass?sourceMap') },
+
+      // Needed for the css-loader when [bootstrap-sass-loader](https://github.com/justin808/bootstrap-sass-loader)
+      // loads bootstrap's css.
+      { test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,  loader: "url?limit=10000&minetype=application/font-woff" },
+      { test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&minetype=application/font-woff" },
+      { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,   loader: "url?limit=10000&minetype=application/octet-stream" },
+      { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,   loader: "file" },
+      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,   loader: "url?limit=10000&minetype=image/svg+xml" }
     ],
   },
   output: {
