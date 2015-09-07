394815bf2a2f277bb916adbc5f39beba02342fe6
Luis Reis <luis.m.reis@gmail.com>
2015-09-07 12:45:54 +0100

    Extract CSS to a separate file.

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 var path = require('path');
 var Clean = require('clean-webpack-plugin');
 var HtmlWebpackPlugin = require('html-webpack-plugin')
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 const distDir = 'dist';
 
@@ -10,18 +11,20 @@ module.exports = {
   ],
   module: {
     loaders: [
-      { test: /\.scss$/, loader: 'style!css!sass' }
+      { test: /\.scss$/, loader: ExtractTextPlugin.extract('css!sass') }
     ],
   },
   output: {
     path: path.join(__dirname, distDir),
-    filename: 'bundle.[hash].js'
+    filename: 'bundle.[hash].js',
+    hashDigestLength: 8
   },
   plugins: [
     new Clean([distDir]),
     new HtmlWebpackPlugin({
       inject: true,
       template: 'app/index.html'
-    })
+    }),
+    new ExtractTextPlugin('styles.[md5:contenthash:8].css')
   ]
 };
